c1caf65c864de418c07aa76fa9bec3b9ca8246d0
eric <collinseric321@gmail.com>
2025-01-19 18:38:55 -0800

    update path

--- data_processing/infraxclimate/nasa_nex/tests/test_infra_intersection.py
+++ data_processing/infraxclimate/nasa_nex/tests/test_infra_intersection.py
@@ -10,7 +10,7 @@ import psycopg2
 import psycopg2.sql as sql
 from pandas.testing import assert_frame_equal
 
-from scenariomip.infra_intersection import (
+from nasa_nex.infra_intersection import (
 
     zonal_aggregation,
     create_pgosm_flex_query,
